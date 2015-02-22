2b2d5ce8efdc521eeccd9cbfb45e833dbf6ff2ad
Daryl Ho <daryl.ho@bluedotsys.com.sg>
2015-02-23 01:16:06 +0800

    Final submission for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,20 +3,20 @@
 
 ## Write a short comment describing this function
 ## This function creates a special "matrix" object that can cache its inverse
-## and also the special trigger indicating if it's inverse values have already
-## been calculated and stored in cache.
+## and also the master trigger/flag (CacheMatrix.Master) indicating if it's 
+## inverse values have already been calculated and stored in cache.
 makeCacheMatrix <- function(x = matrix()) {
-     inv <- NULL
-     set <- function(y) {
+     CacheMatrix.Master <- NULL 
+     CacheMatrix.New <- function(y) {
           x <<- y
-          inv <<- NULL
+          CacheMatrix.Master <<- NULL
      }
-     get <- function() x
-     setinverse <- function(inverse) inv <<- inverse
-     getinverse <- function() inv
-     list(set = set, get = get,
-          setinverse = setinverse,
-          getinverse = getinverse)
+     CacheMatrix.Values <- function() x
+     CacheMatrix.InverseProcess <- function(z) CacheMatrix.Master <<- z
+     CacheMatrix.InversedValues <- function() CacheMatrix.Master
+     list(CacheMatrix.New = CacheMatrix.New, CacheMatrix.Values = CacheMatrix.Values,
+          CacheMatrix.InverseProcess = CacheMatrix.InverseProcess,
+          CacheMatrix.InversedValues = CacheMatrix.InversedValues)
 }
 
 
@@ -25,13 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## makeCacheMatrix and checks if the inverse values have already been
 ## calculated and if so to retrieve the values directly from cache
 cacheSolve <- function(x) {
-     inv <- x$getinverse()
-     if(!is.null(inv)) {
+     CacheMatrix.Master <- x$CacheMatrix.InversedValues()
+     if(!is.null(CacheMatrix.Master)) {
           message("getting cached data")
-          return(inv)
+          return(CacheMatrix.Master)
      }
-     data <- x$get()
-     inv <- solve(data)
-     x$setinverse(inv)
-     inv
+     Matrixdata <- x$CacheMatrix.Values()
+     CacheMatrix.Master <- solve(Matrixdata)
+     x$CacheMatrix.InverseProcess(CacheMatrix.Master)
+     CacheMatrix.Master
 }
